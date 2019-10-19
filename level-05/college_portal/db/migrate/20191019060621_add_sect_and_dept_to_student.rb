0785c4f83ad9c41a23babb5ecda3519dddbcc36b
pilgrimm13 <52787530+pilgrimm13@users.noreply.github.com>
2019-10-19 14:00:47 +0530

    Add files via upload

---
+++ level-05/college_portal/db/migrate/20191019060621_add_sect_and_dept_to_student.rb
@@ -0,0 +1,6 @@
+class AddSectAndDeptToStudent < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :students, :department_id, :integer
+  	add_column :students, :section_id, :integer
+  end
+end
